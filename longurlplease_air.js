73b8b53d8ca533c934378c14f926f94dfcea5450
Darragh Curran <darragh@peelmeagrape.net>
2008-12-30 23:01:59 +0000

    extract some methods to explain what's happening a bit

--- longurlplease_air.js
+++ longurlplease_air.js
@@ -2,52 +2,60 @@ var longurlplease = {
   // At the moment clients must maintain a list of services which they will attempt to lengthen short urls for
   shortUrlsPattern : new RegExp("^http(s?)://(adjix\\.com|bit\\.ly|dwarfurl\\.com|ff\\.im|idek\\.net|is\\.gd|ln-s\\.net|loopt\\.us|ping\\.fm|piurl\\.com|piurl\\.com|poprl\\.com|qlnk\\.net|reallytinyurl\\.com|rubyurl\\.com|short\\.ie|smallr\\.com|snipr\\.com|snipurl\\.com|snurl\\.com|tinyurl\\.com|tr\\.im|twurl\\.nl|ub0\\.cc|ur1\\.ca|url\\.ie|urlx\\.ie|xrl\\.us|yep\\.it|zi\\.ma|zurl\\.ws)/.*"),
   numberOfUrlsPerBatch : 4,
+  urlToElement : {},
   lengthen : function(element) {
     var parent = (element == null) ? document : element ;
     var toLengthen = [];
-    var urlToElement = {};
     var els = parent.getElementsByTagName('a');
     for (var elIndex = 0; elIndex < els.length; elIndex++) {
       var el = els[elIndex];
       if (longurlplease.shortUrlsPattern.test(el.href)) {
         toLengthen.push(el.href);
-        var listOfElements = urlToElement[el.href];
+        var listOfElements = longurlplease.urlToElement[el.href];
         if (listOfElements == null)
           listOfElements = []
         listOfElements.push(el);
-        urlToElement[el.href] = listOfElements;
+        longurlplease.urlToElement[el.href] = listOfElements;
       }
     }
     var subArray, i = 0;
     while (i < toLengthen.length) {
       subArray = toLengthen.slice(i, i + longurlplease.numberOfUrlsPerBatch)
-      var paramString = "";
-      for (var j = 0; j < subArray.length; j++) {
-        var href = subArray[j];
-        paramString += "q=";
-        paramString += encodeURI(href);
-        if (j < subArray.length - 1)
-          paramString += '&'
-      }
-      var loader = new air.URLLoader();
-      loader.addEventListener(air.Event.COMPLETE,
-              function (event) {
-                var data = event.target.data;
-                JSON.parse(data, function (key, val) {
-                  if (typeof val === 'string' && val != null) {
-                    var aTags = urlToElement[key];
-                    for (var ai = 0; ai < aTags.length; ai++) {
-                      aTag = aTags[ai];
-                      longurlplease.alterLink(aTag, val);
-                    }
-                  }
-                });
-              }
-              );
-      var request = new air.URLRequest("http://www.longurlplease.com/api/v1.1?ua=air&" + paramString);
-      loader.load(request);
+      var paramString = longurlplease.toParamString(subArray);
+      longurlplease.makeRequest(paramString);
       i = i + longurlplease.numberOfUrlsPerBatch;
     }
+  },
+  toParamString : function(shortUrls) {
+    var paramString = "";
+    for (var j = 0; j < shortUrls.length; j++) {
+      var href = shortUrls[j];
+      paramString += "q=";
+      paramString += encodeURI(href);
+      if (j < shortUrls.length - 1)
+        paramString += '&'
+    }
+    return paramString;
+  },
+  makeRequest : function(paramString) {
+    var loader = new air.URLLoader();
+    loader.addEventListener(air.Event.COMPLETE,
+            function (event) {
+              longurlplease.handleJsonResponse(event.target.data);
+            });
+    var request = new air.URLRequest("http://www.longurlplease.com/api/v1.1?ua=air&" + paramString);
+    loader.load(request);
+  },
+  handleJsonResponse : function(response) {
+    JSON.parse(response, function (key, val) {
+      if (typeof val === 'string' && val != null) {
+        var aTags = longurlplease.urlToElement[key];
+        for (var ai = 0; ai < aTags.length; ai++) {
+          aTag = aTags[ai];
+          longurlplease.alterLink(aTag, val);
+        }
+      }
+    });
   },
   alterLink : function(a, longUrl) {
     // You can customize this - my intention here is to alter the visible text to use as much of the long url
